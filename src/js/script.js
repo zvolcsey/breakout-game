01dcbe382c0f903e95448f55a87458b8d81407bb
Zoltán Völcsey <zvolcsey@gmail.com>
2022-06-28 03:22:55 +0200

    creating and styling the page

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+
+// Rules and close event listeners
+rulesBtn.addEventListener('click', () => {
+  rules.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  rules.classList.remove('show');
+});
